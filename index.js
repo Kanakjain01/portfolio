aab874739819f9d0d9a40e340f8ea73ea747f9f7
kanak <kanakjain639@gmail.com>
2023-02-01 12:44:31 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log("srg")
+// document.querySelector("nav").addEventListener("mouseover",()=>{
+//     document.querySelector("nav").style.backgroundColor="blue"
+//     console.log("here")
+// })
+
+// document.querySelector("nav").addEventListener("mouseout",()=>{
+//     document.querySelector("nav").style.backgroundColor="yellow"
+//     console.log("here")
+// })
